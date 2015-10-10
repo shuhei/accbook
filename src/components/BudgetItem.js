3c49adf9c76bd83a84c01b94ece28a0833a108f9
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-10-10 16:42:26 +0900

    Remove edit button making the entire row clickable.

--- src/components/BudgetItem.js
+++ src/components/BudgetItem.js
@@ -11,16 +11,11 @@ export default function BudgetItem({ item, subtotal, editItem }) {
     'budget-item--deficit': subtotal < 0
   });
   return (
-    <div className={className}>
+    <div className={className} onClick={() => editItem(item)}>
       <div className="grid-1-6 budget-item__date">{formatDate(item.date)}</div>
       <div className="grid-1-6 budget-item__amount">{formatNumber(item.amount)}</div>
       <div className="grid-1-6 budget-item__total">{formatNumber(subtotal)}</div>
-      <div className="grid-1-4 budget-item__label">{item.label}</div>
-      <div className="grid-1-4 budget-item__tools">
-        <button className="button button--small" onClick={() => editItem(item)}>
-          <i className="fa fa-pencil" />
-        </button>
-      </div>
+      <div className="grid-1-2 budget-item__label">{item.label}</div>
     </div>
   );
 }
